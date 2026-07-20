4155f992c7a6b100fff1acde2b84268fa89769d3
Eliza Dubiel <dubieleliza89@gmail.com>
2026-07-20 09:21:32 +0200

    Type Annotations

---
+++ typeAnnotations-exercise.ts
@@ -0,0 +1,16 @@
+// Type Annotations Practice
+//Create variables with explicit types:
+// Create a variable called `projectName` with type string.
+//Create a variable called `testCount` with type number.
+// Create a variable called `isTestCompleted` with type boolean.
+// Print all variables to the console.
+
+let projectName: string = "TS for begginer";
+let testCount: number = 4;
+let isTestCompleted:boolean = true; 
+
+
+console.log(projectName);
+console.log(testCount);
+console.log(isTestCompleted);
+
---
+++ typeAnnotations.ts
@@ -0,0 +1,13 @@
+//Type annotations define the data type a variable. They help catch errors early and make the code easier to read. 
+// Common types: string, number and boolean. 
+
+let username: string = "admin";
+
+//Example for QA
+let username: string = "Tester";
+let password: string = "password123";
+let timeout: number = 5000;
+let productionCount: number = 3;
+let isLoggedIn: boolean = true;
+let isButtonVisible: boolean = false;
+
