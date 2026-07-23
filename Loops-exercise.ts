cb47e29ed408882f69fef5dbe75880a6e9d0511e
Eliza Dubiel <dubieleliza89@gmail.com>
2026-07-23 13:46:46 +0200

    Loops

---
+++ Loops-exercise.ts
@@ -0,0 +1,48 @@
+/* Use a for loop.
+Print numbers from 1 to 10. */
+
+for (let i=1; i>=10; i++) {
+     console.log(i);
+}
+
+/* Use a for loop.
+Print numbers from 10 to 1. */
+
+for (let i=10; i<=1; i--) {
+    console.log(i);
+}
+
+/* Use a for loop.
+Print only even numbers from 2 to 20. */
+
+for (let i=2; i<=20; i++) {
+    if(i % 2 ===0) {
+    console.log(i);
+    }
+}   
+
+/* Exercise 4
+Use a while loop.
+Print numbers from 1 to 5. */
+
+let number = 1;
+while(number<=5) {
+    console.log(number);
+    number++;
+}
+
+/* Exercise 5
+Create an array:
+const fruits = ["Apple", "Banana", "Orange", "Mango"];
+Use a for...of loop.
+Print every fruit. */
+
+const fruts = [
+    "Apple",
+    "Banana",
+    "Kiwi",
+    "Pear",
+]
+for(const fruit of fruts) {
+    console.log(fruts);
+}
---
+++ Loops.ts
@@ -0,0 +1,50 @@
+// for
+
+for (let i =1; i<= 5; i++) {
+    console.log(i);
+}
+
+//Loop through an array 
+
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange"
+];
+
+for (let i =0; i<fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+//for .. of 
+const vagetable = [
+    "Tomato",
+    "Cucumber",
+    "Potato",
+];
+
+for (count vagetable of vagetable) {
+    console.log(vagetable);
+}
+
+//Break
+
+for (let i=1; i<=10; i++) {
+    if(i=== 5) {
+        break;
+    }
+    console.log(i);
+}
+
+//continue
+
+for (let i =1; i<=5; i++) {
+    if (i===3) {
+        continue;
+    }
+    console.log(i)
+}
+
+
+
+
